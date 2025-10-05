b95c14dcc54d7caafbc9019f9e45c3a5ca67b861
Haak8267 <haroldaannancy@gmail.com>
2025-10-05 12:56:26 +0000

    Node

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express")
+
+//Intialization
+const server = express()
+
+//controller
+const Homepage = (req,res)=>{
+    res.send("This is the home page")
+}
+
+const Products = (req,res)=>{
+    res.send("This is my Product page")
+}
+
+const Gallery = (req,res)=>
+    res.send("Pictures from the Gallery")
+
+const About = (req,res)=>{
+    res.send("This is my About Page")
+}
+
+const Contact = (req,res) =>{
+    res.send("This is my Contact page")
+}
+
+
+//routes
+server.use('/Products',Products)
+server.use('/Gallery', Gallery)
+server.use('/About', About)
+server.use('/Contact', Contact)
+server.use('/',Homepage)
+
+
+// starting server
+server.listen(3000, ()=> console.log("server is working on port 3000"))
